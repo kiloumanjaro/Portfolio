a91ae9f4ac87a6799e660e87ee3786e2dcc8bc3a
Kint <kiloumanjaro@gmail.com>
2025-06-15 20:57:10 +0800

    added vuetify plugin

---
+++ src/components/layouts/TechstackSection.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Techstack</h1>
+    <div class="inline-flex items-center"></div>
+  </div>
+</template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import vuetify from 'vite-plugin-vuetify'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss(), vueDevTools()],
+  plugins: [vue(), tailwindcss(), vueDevTools(), vuetify({ autoImport: true })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
